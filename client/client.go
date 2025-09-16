4326c8e6c56d42977753467cb6f84abea25f71e3
Chernovuk <cherpash20@mail.ru>
2025-09-16 14:37:29 +0300

    feat(client): Write funcs to connect client to url and to check response from the server.

--- client/client.go
+++ client/client.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
+var url = "http://localhost:8080"
+
 func main() {
 	client := NewClient()
+	ConnectClientToUrl(client)
 }
 
 func NewClient() *http.Client {
@@ -18,4 +24,24 @@ func NewClient() *http.Client {
 		Transport: tr,
 	}
 }
+
+func ConnectClientToUrl(cl *http.Client) {
+	resp, err := cl.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Default().Println(err)
+	}
+	if !checkResponse(string(body)) {
+		fmt.Println("Wrong response!")
+		return
+	}
+	fmt.Println("Successful one-time connection")
+}
+
+func checkResponse(resp string) bool {
+	return resp == "OK\n"
 }
